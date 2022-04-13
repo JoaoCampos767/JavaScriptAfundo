d3c6a64cd600d8dd0a28a4dc04be11d92f4543f4
Joao <joaopedro767767@gmail.com>
2022-04-13 19:54:44 -0300

    Ajuste

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -47,7 +47,7 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
     new ParDeBarreiras(altura, abertura, largura + espaco * 3),
   ];
 
-  const deslocamento = 3;
+  const deslocamento = 6;
   this.animar = () => {
     this.pares.forEach((par) => {
       par.setX(par.getX() - deslocamento);
