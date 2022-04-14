b2637a2a9abb5cefc926ec6df138c8f529ecd7fd
João Pedro Campos <joaopedro767767@gmail.com>
2022-04-13 22:14:04 -0300

    Continuando estudo webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require("webpack");
+const MiniCssEstractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -6,5 +7,22 @@ module.exports = {
   output: {
     filename: "principal.js",
     path: __dirname + "/public",
+  },
+  plugins: [
+    new MiniCssEstractPlugin({
+      filename: "estilo.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssEstractPlugin.loader,
+          // "style-loader", // Adiciona CSS a DOM injetando a tag <style>
+          "css-loader", // interpretar @import, url()
+        ],
+      },
+    ],
   },
 };
